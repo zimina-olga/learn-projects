977b3ac29781be274b375958c53807e00105b351
zimina-olga <olka.v.zimina@gmail.com>
2021-03-11 16:05:25 +0300

    guide 2

--- consuming-rest/src/main/java/com/example/consumingrest/ConsumingRestApplication.java
+++ consuming-rest/src/main/java/com/example/consumingrest/ConsumingRestApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ConsumingRestApplication {
-// Check 1
+
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumingRestApplication.class, args);
 	}
