a8b8daf9314f1ec76702f01596ad3eb59ac70bfb
zimina-olga <olka.v.zimina@gmail.com>
2021-03-16 11:04:45 +0300

    guide 2 experimental

--- consuming-rest/src/main/java/com/example/consumingrest/Value.java
+++ consuming-rest/src/main/java/com/example/consumingrest/Value.java
@@ -1,12 +1,13 @@
 package com.example.consumingrest;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Value {
 
     private Long id;
-    private String quote;
+    private String quote1;
 
     public Value() {
     }
@@ -15,23 +16,25 @@ public class Value {
         return this.id;
     }
 
-    public String getQuote() {
-        return this.quote;
+    @JsonProperty("quote")
+    public String getQuote1() {
+        return this.quote1;
     }
 
     public void setId(Long id) {
         this.id = id;
     }
 
-    public void setQuote(String quote) {
-        this.quote = quote;
+    @JsonProperty("quote")
+    public void setQuote1(String quote) {
+        this.quote1 = quote;
     }
 
     @Override
     public String toString() {
         return "Value{" +
                 "id=" + id +
-                ", quote='" + quote + '\'' +
+                ", quote='" + quote1 + '\'' +
                 '}';
     }
 }
