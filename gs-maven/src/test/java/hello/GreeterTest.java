50e569421389021f9ef4ec0330c265e5547c55c5
zimina-olga <olka.v.zimina@gmail.com>
2021-03-23 12:28:13 +0300

    Building Java Projects with Maven

---
+++ gs-maven/src/test/java/hello/GreeterTest.java
@@ -0,0 +1,18 @@
+package hello;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GreeterTest {
+
+    private Greeter greeter = new Greeter();
+
+    @Test
+    public void greeterSaysHello() {
+        assertThat(greeter.sayHello(), containsString("Hello"));
+        System.out.println(greeter.sayHello()+ "  contains Hello");
+    }
+
+}
